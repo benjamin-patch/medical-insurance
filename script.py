ae4ac9d33c472b3d7c6a96ce463e14178ba358cf
Benjamin Patch <ben@benjaminpatch.com>
2025-01-25 17:23:01 -0800

    Calculate BMI factor.

--- script.py
+++ script.py
@@ -21,6 +21,14 @@ change_in_insurance_cost = new_insurance_cost - insurance_cost
 print("The change in cost of insurance after increasing the age by 4 years is", change_in_insurance_cost, "dollars.")
 
 # BMI Factor
+age = 28
+bmi += 3.1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated insurance cost after increasing BMI by 3.1 is", change_in_insurance_cost, "dollars.")
 
 
 # Male vs. Female Factor
