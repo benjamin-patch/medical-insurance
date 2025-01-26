d2db666e0fc8d30ddb71bc2434e7af4cbb97331c
Benjamin Patch <ben@benjaminpatch.com>
2025-01-25 16:50:22 -0800

    Create and print insurance estimate formula.

--- script.py
+++ script.py
@@ -7,7 +7,9 @@ smoker = 0
 
 
 # Add insurance estimate formula below
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 
+print("This person's insurance cost is", insurance_cost, "dollars.")
 
 # Age Factor
 
