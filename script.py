b54bff98e4e7b38869dfe3050bbaaa63c0d07a39
Benjamin Patch <ben@benjaminpatch.com>
2025-01-25 17:13:23 -0800

    Calculate the age factor into insurance costs.

--- script.py
+++ script.py
@@ -12,7 +12,13 @@ insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 240
 print("This person's insurance cost is", insurance_cost, "dollars.")
 
 # Age Factor
+age += 4
 
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in cost of insurance after increasing the age by 4 years is", change_in_insurance_cost, "dollars.")
 
 # BMI Factor
 
