83a6e0c082349de9702a742defca7fd9f8e31068
Benjamin Patch <ben@benjaminpatch.com>
2025-02-02 15:24:02 -0800

    Plug Maria's variables into calculate_insurance_cost() function.

--- function_estimator.py
+++ function_estimator.py
@@ -3,18 +3,10 @@ def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker):
     estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
     print("The estimated insurance cost for this person is", estimated_cost, "dollars.")
     return estimated_cost
-
-# Initial variables for Maria 
-age = 28
-sex = 0  
-bmi = 26.2
-num_of_children = 3
-smoker = 0  
-
+ 
+ 
 # Estimate Maria's insurance cost
-insurance_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-
-print("The estimated insurance cost for Maria is " + str(insurance_cost) + " dollars.")
+maria_insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0)
 
 # Initial variables for Omar
 age = 35
