56ae058d2f272de8a14ac81eec231684c47a79b3
Benjamin Patch <ben@benjaminpatch.com>
2025-02-04 17:23:42 -0800

    Add conditional statements based on smoking status.

--- conditional_costs.py
+++ conditional_costs.py
@@ -1,7 +1,10 @@
-# conditional costs
+# analyze smoker status
 def analyze_smoker(smoker_status):
-  print(smoker_status)
- 
+  if smoker_status == 1:
+    print("To lower your cost, you should consider quitting smoking.")
+  else:
+    print("Smoking is not an issue for you.")
+
 # estimate insurance cost
 def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
   estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
