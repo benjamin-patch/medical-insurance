4f2b680e1da9dd9a0448986466cc4f8cf547596b
Benjamin Patch <ben@benjaminpatch.com>
2025-02-04 17:03:25 -0800

    Initial commit of 'conditional_costs.py'

---
+++ conditional_costs.py
@@ -0,0 +1,11 @@
+# conditional costs
+
+ 
+# estimate insurance cost
+def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
+  estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  return estimated_cost
+ 
+# estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, num_of_children = 3, smoker = 1)
