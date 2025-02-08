7b4079e0cb0311d3ce3288ef10d24054382370dc
Benjamin Patch <ben@benjaminpatch.com>
2025-02-07 16:28:49 -0800

    Consolidate estimated insurance data.

--- insurance_estimation_lists.py
+++ insurance_estimation_lists.py
@@ -21,3 +21,12 @@ insurance_costs = [4150.0, 5320.0, 35210.0]
 
 insurance_data = list(zip(names, insurance_costs))
 print(insurance_data)
+
+# consolidate estimated insurance data
+estimated_insurance_data = []
+
+estimated_insurance_data.append(("Maria", maria_insurance_cost))
+estimated_insurance_data.append(('Rohan', rohan_insurance_cost))
+estimated_insurance_data.append(('Valentina', valentina_insurance_cost))
+
+print(estimated_insurance_data)
