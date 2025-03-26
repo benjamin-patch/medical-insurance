19765e137459da3a4eb841fd5102c66f0bd88927
Benjamin Patch <ben@benjaminpatch.com>
2025-03-26 14:27:51 -0700

    Correct the value of Vinay's record.

--- dictionary_records.py
+++ dictionary_records.py
@@ -1,9 +1,14 @@
+# initial records
 medical_costs = {'Marina': 6607.0, 'Vinay': 3225.0}
 
+# add records
 medical_costs.update({
                       'Connie': 8886.0,
                       'Isaac': 16444.0,
                       'Valentina': 6420.0
                     })
+# correct the value of Vinay's record
+medical_costs['Vinay'] = 3325.0
 
+# output
 print(medical_costs)
