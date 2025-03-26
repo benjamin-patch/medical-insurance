405ee3961d823cbec2f1e21d3695a0b0a17649a0
Benjamin Patch <ben@benjaminpatch.com>
2025-03-26 14:11:47 -0700

    Add records with 'update()' method.

--- dictionary_records.py
+++ dictionary_records.py
@@ -1,1 +1,9 @@
 medical_costs = {'Marina': 6607.0, 'Vinay': 3225.0}
+
+medical_costs.update({
+                      'Connie': 8886.0,
+                      'Isaac': 16444.0,
+                      'Valentina': 6420.0
+                    })
+
+print(medical_costs)
