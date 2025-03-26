234c3fe58d03b2598dcfa6724075dcc978d83fd5
Benjamin Patch <ben@benjaminpatch.com>
2025-03-26 16:40:12 -0700

    Create new dictionary for medical records.

--- dictionary_records.py
+++ dictionary_records.py
@@ -25,7 +25,16 @@ zipped_ages = zip(names, ages)
 names_to_ages = {key:value for key, value in zipped_ages}
 # check Marina's age
 marina_age = names_to_ages.get('Marina', None)
-print("Marina's age is", marina_age)
+# print("Marina's age is", marina_age)
+
+# create new dictionary for medical records
+medical_records = {}
+# add to medical_records dictionary
+medical_records["Marina"] = {"Age": 27, "Sex": "Female", "BMI": 31.1, "Children": 2, "Smoker": "Non-smoker", "Insurance_cost": 6607.0}
+medical_records["Vinay"] = {"Age": 24, "Sex": "Male", "BMI": 26.9, "Children": 0, "Smoker": "Non-smoker", "Insurance_cost": 3225.0}
+medical_records["Connie"] = {"Age": 43, "Sex": "Female", "BMI": 25.3, "Children": 3, "Smoker": "Non-smoker", "Insurance_cost": 8886.0}
+medical_records["Isaac"] = {"Age": 35, "Sex": "Male", "BMI": 20.6, "Children": 4, "Smoker": "Smoker", "Insurance_cost": 16444.0}
+medical_records["Valentina"] = {"Age": 52, "Sex": "Female", "BMI": 18.7, "Children": 1, "Smoker": "Non-smoker", "Insurance_cost": 6420.0}
 
 # output
 # print(medical_costs)
